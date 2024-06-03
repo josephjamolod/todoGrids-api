c0c1735be37090b989cac70d7b309495a16f74fe
Joseph Jamolod <josephjamolod@gmail.com>
2024-06-03 12:10:42 +0800

    change

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  exposedHeaders: ["Set-Cookie"],
 };
 
 app.use(cors(corsOptions));
